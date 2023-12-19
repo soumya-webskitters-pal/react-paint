7bff64c773d225089aac3b86363589576a0479ec
soumya <soumya.pal@webskitters.com>
2023-12-19 20:08:11 +0530

    asd

--- myPaint/src/assets/components/CreateTools.jsx
+++ myPaint/src/assets/components/CreateTools.jsx
@@ -6,9 +6,10 @@ import BrushInfo from "./BrushInfo";
 import CreateResetCanvas from "./CreateResetCanvas";
 import CreateSaveAs from "./CreateSaveAs";
 import CreateTips from "./CreateTips";
+import DrawPointer from "./CreatePointer";
 
 function CreateTools() {
-  return <><CreateColorPallet/><CreateDrawingBox/><BrushInfo/><CreateResetCanvas/><CreateSaveAs/><CreateTips/></>
+  return <><CreateColorPallet/><CreateDrawingBox/><BrushInfo/><CreateResetCanvas/><CreateSaveAs/><CreateTips/><DrawPointer/></>
 }
 
 export default CreateTools;
